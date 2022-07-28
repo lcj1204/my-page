6afbf22236587dbe334db56a5860d297c119ec7b
lcj1204 <dlckdwo1204@naver.com>
2022-07-28 20:06:00 +0900

    Initial commit

---
+++ src/test/java/hello/mypage/MyPageApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.mypage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
