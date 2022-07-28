39912c2de99ee927b408d32a21df863b63ee4c89
lcj1204 <dlckdwo1204@naver.com>
2022-07-28 21:38:52 +0900

    member 도메인 생성

---
+++ src/main/java/hello/mypage/domain/member/Member.java
@@ -0,0 +1,14 @@
+package hello.mypage.domain.member;
+
+public class Member {
+
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String alias;
+
+    private String passwd;
+}
